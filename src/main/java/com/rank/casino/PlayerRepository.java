c88c6edc00d2b65e79f3e0fe916c2366693b5635
takealotkim <kim.birk@takealot.com>
2021-05-05 22:27:22 +0200

    add player lookup by username

--- src/main/java/com/rank/casino/PlayerRepository.java
+++ src/main/java/com/rank/casino/PlayerRepository.java
@@ -1,6 +1,7 @@
 package com.rank.casino;
 
 import com.rank.casino.model.Player;
+import com.rank.casino.model.RequestInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -14,4 +15,6 @@ interface PlayerRepository extends JpaRepository<Player, Long> {
     @Query(value = "UPDATE Player p SET p.balance = balance + ?1 WHERE p.ID = ?2",
             nativeQuery = true)
     void updateBalance(Double amount, Long playerId);
+
+    Player findByUsername(String username);
 }
